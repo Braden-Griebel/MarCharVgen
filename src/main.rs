e0b409dbf49af65c9db3e48a066d57c186d1bf1a
Braden Griebel <bgriebel@uw.edu>
2025-03-07 14:15:54 -0800

    Completed main cli

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,8 @@ mod generator;
 mod sampling;
 // Includes
 // Standard Library
-use std::io::{self, BufRead, Read};
+use std::fs::{self, File};
+use std::io::{self, LineWriter, Read, Write};
 use std::path::PathBuf;
 // External Crates
 use clap::Parser;
@@ -40,6 +41,7 @@ struct Cli {
 
 fn main() -> Result<(), anyhow::Error> {
     let cli = Cli::parse();
+    // Create a generator either based on file contents or stdin
     let generator: MarkovGenerator = match cli.file {
         Some(f) => MarkovGenerator::from_file(f, cli.depth)?,
         None => {
@@ -50,6 +52,45 @@ fn main() -> Result<(), anyhow::Error> {
             MarkovGenerator::from_vec(&buffer.lines().collect::<Vec<&str>>(), cli.depth)?
         }
     };
+    // If the output needs to go to a file, open that file and write line by line
+    if let Some(f) = cli.outfile {
+        // Open the file, and create a LineWriter
+        let file = File::create(f)?;
+        let mut writer = LineWriter::new(file);
+        // Loop until number of desired words is generated
+        for _ in 0..cli.wordcount {
+            // Generate a new word, then remove the first and last
+            // char, which will be STARTCHAR, and ENDCHAR
+            let new_word = generator.generate()?;
+            let mut chars = new_word.chars();
+            chars.next();
+            chars.next_back();
+            let mut new_word = chars.collect::<String>();
+            // Add a new line to the end of the string
+            new_word.push('\n');
+            // Write to the file
+            writer.write_all(new_word.as_bytes())?;
+        }
+    } else {
+        // Create a String which will hold all the output
+        let mut output_string: String = String::new();
+        // Loop until number of desired words generated
+        for _ in 0..cli.wordcount {
+            // Generate a new word, then remove the first and last
+            // char, which will be STARTCHAR, and ENDCHAR
+            let new_word = generator.generate()?;
+            let mut chars = new_word.chars();
+            chars.next();
+            chars.next_back();
+            let mut new_word = chars.collect::<String>();
+            // Append a newline to the end after the word
+            new_word.push('\n');
+            // Add the new word to the end of the output
+            output_string.push_str(&new_word);
+        }
+        // Write the output to the STDOUT
+        print!("{}", output_string);
+    }
 
     Ok(())
 }
